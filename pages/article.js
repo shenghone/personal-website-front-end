e05ef6ebef27ccbe93754106ce3db212692e761d
shenghone <shenghone@gmail.com>
2019-11-13 00:46:32 -0500

    fix blog connection problem

--- pages/article.js
+++ pages/article.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useRouter } from "next/router";
 import ArticleBody from "../components/ArticleBody";
 
 const article = props => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
   console.log(props);
   return <Layout>{/*<ArticleBody articleId={props.articleId} />*/}</Layout>;
-};
-
-article.getInitialProps = async ({ query: { id } }) => {
-  return { articleId: id };
 };
 
 export default article;
