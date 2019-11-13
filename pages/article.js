a33917532c2806d010a6cc95c6f1965c213ab4d0
shenghone <shenghone@gmail.com>
2019-11-12 23:39:25 -0500

    fix blog connection problem

--- pages/article.js
+++ pages/article.js
@@ -2,14 +2,14 @@ import React from "react";
 import Layout from "../components/Layout";
 import ArticleBody from "../components/ArticleBody";
 
-const article = React.memo(function(props) {
+const article = props => {
   console.log(props);
   return (
     <Layout>
       <ArticleBody articleId={props.articleId} />
     </Layout>
   );
-});
+};
 
 article.getInitialProps = async ({ query: { id } }) => {
   return { articleId: id };
